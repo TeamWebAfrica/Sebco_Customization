188101d5d491d9e5f79e437eef4caf33f775911a
kipngetich <kipngetich@finplusgroup.com>
2023-09-08 14:27:05 +0300

    Add Boya API logger

--- sebco_customization/sebco_customization/custom_methods/api_methods.py
+++ sebco_customization/sebco_customization/custom_methods/api_methods.py
@@ -1,8 +1,13 @@
 import frappe
 from sebco_customization.sebco_customization.custom_methods.boya_payments import BoyaPayments
 
+frappe.utils.logger.set_log_level("DEBUG")
+logger = frappe.logger("boya_webhook", allow_site=True, file_count=50)
+
 @frappe.whitelist(allow_guest=True)
 def boya_payments_api(*args, **kwargs):
+    logger.info('==========================================================================================================================')
+    logger.info(f"Boya Webhook Request: \n args: {args} , \n kwargs: {kwargs}")
     # hardcode user to administrator
     frappe.session.user = 'Administrator'
    
